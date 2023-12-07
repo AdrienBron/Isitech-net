74d2b7c803e41c46464d6c12d600a7647021c9e0
Adrien_Bron <adrienbron2003@gmail.com>
2023-12-07 11:46:03 +0100

    decorateurs

--- WEBAPI/Models/Book.cs
+++ WEBAPI/Models/Book.cs
@@ -3,7 +3,6 @@ using System.ComponentModel.DataAnnotations;
 
 public class Book : BaseModel
 {
-    [Required]
     public int Id { get; set; }
     public string? Title { get; set; }
     public string? Author { get; set; }
